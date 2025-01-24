94c84a9f586cd9655ab2a328740340a42c34dd44
221B217 <221B217@juet.co.in>
2025-01-24 14:22:57 +0530

    Lab1 Commited

---
+++ LE1/Problem3/Main.java
@@ -0,0 +1,10 @@
+class Main {
+	public static void main (String args[]) {
+		Mother m = new Mother ( );
+		m.show( );
+		Mother m1 = new Mother ( );
+		m1.show( );
+		Child ch=new Child ( );
+		ch.show ( );
+	}
+}
