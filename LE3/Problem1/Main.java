5e99a7d559ed3bf8b5d6eba167bcad2a27ece3c0
221B217 <221B217@juet.co.in>
2025-02-07 15:06:46 +0530

    LE3 Updated!

---
+++ LE3/Problem1/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Arjun arjun = new Arjun();
+        Bheem bheem = new Bheem();
+        Kaurav duryodhan = new Kaurav();
+        Vikarn vikarn = new Vikarn();
+
+        arjun.fight();
+        bheem.obey();
+        bheem.kind();
+        duryodhan.fight();
+        vikarn.obey();
+        vikarn.kind();
+    }
+}
---
+++ LE3/Problem2/Main.java
@@ -0,0 +1,12 @@
+interface Testable {
+    void display(); 
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.display();
+        FinalTest abstest = new FinalTest();
+        abstest.display();
+    }
+}
